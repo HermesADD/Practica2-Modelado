ffc970c59a1d3f788dd367e3f7b6f4da0a932c2d
steve-quezada <80070698+steve-quezada@users.noreply.github.com>
2024-03-08 16:53:15 -0600

    Implementación interface EstadoRobot

---
+++ Practica2/src/Robots/EstadoRobot.java
@@ -0,0 +1,47 @@
+package Robots;
+
+/**
+ * La Interfaz EstadoRobot define los métodos que deben ser implementados por los 
+ * diferentes estados de un robot. Estos métodos corresponden a las acciones que 
+ * el robot puede realizar y los cambios de estado que puede experimentar.
+ */
+public interface EstadoRobot {
+    
+    /**
+     * Activa el robot desde un estado suspendido.
+     */
+    public void activar();
+
+    /**
+     * Suspende el robot, deteniendo sus actividades.
+     */
+    public void suspender();
+
+    /**
+     * Hace que el robot comience a caminar.
+     */
+    public void caminar();
+
+    /**
+     * Permite al robot tomar la orden de un cliente.
+     */
+    // public void tomarOrden(Menu[] listaMenus);
+    public void tomarOrden();
+
+    /**
+     * Inicia el proceso de cocinar un platillo.
+     */
+    public void cocinar();
+
+    /**
+     * Permite al robot servir un platillo a un cliente.
+     */
+    public void servir();
+
+    /**
+     * Imprime el estado actual del robot, proporcionando información sobre su 
+     * actividad o estado de reposo.
+     */
+    public void imprimirEstado();
+    
+}
